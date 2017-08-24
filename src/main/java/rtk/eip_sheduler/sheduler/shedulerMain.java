a5f204f6d938f622cff06500c0db791c72ead954
vasil <andr_vasil@mail.ru>
2017-08-24 16:38:31 +0300

    Рабочая версия

--- src/main/java/rtk/eip_sheduler/sheduler/shedulerMain.java
+++ src/main/java/rtk/eip_sheduler/sheduler/shedulerMain.java
@@ -5,9 +5,12 @@
  */
 package rtk.eip_sheduler.sheduler;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 import org.apache.log4j.Logger;
-
-
+import rtk.eip_sheduler.DAO.TUsersLogDAO;
+import rtk.eip_sheduler.beans.TUsersLog;
 
 /**
  *
@@ -17,11 +20,17 @@ public class shedulerMain {
 
     /**
      * @param args the command line arguments
-     */
+     */    
+
     public static void main(String[] args) {
         // TODO code application logic here
         Logger log = Logger.getLogger(shedulerMain.class.getName());
         log.info("Start");
+        EntityManager em = Persistence.createEntityManagerFactory("EIP_shaduler_eip_sheduler_jar_1PU").createEntityManager();
+        List<TUsersLog> logItems = (new TUsersLogDAO(em)).getList();
+        for (TUsersLog item : logItems) {
+            log.info(item);
+        }
     }
-    
+
 }
