d5d81d1bbe0a0d485d4faf1c977223306d22551f
vasil <andr_vasil@mail.ru>
2017-08-24 18:01:03 +0300

    Рабочая версия

---
+++ src/main/java/rtk/eip_sheduler/httpUtil/utlHttp.java
@@ -0,0 +1,231 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rtk.eip_sheduler.httpUtil;
+
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Map;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicHeader;
+
+/**
+ *
+ * @author vasil
+ */
+public class utlHttp {
+
+    public String doPost(String url, Object params, Map<String, String> headerList) {
+        System.out.println("doPost => " + params.toString());
+        String res = null;
+        try {
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            Gson gson = new Gson();
+
+            HttpPost post = new HttpPost(url);    
+            // Добавляем данные в формате JSON
+            StringEntity postingString = new StringEntity(gson.toJson(params), "application/json", "UTF-8");          
+            post.setEntity(postingString);
+
+            if (headerList != null) {
+                headerList.entrySet().stream().forEach((t) -> {
+                    Header header = new BasicHeader(t.getKey(), t.getValue());
+                    //System.out.println("header => " + header);
+                    post.setHeader(header);
+                });
+            }
+            HttpResponse response = httpClient.execute(post);
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            String line = "";
+            StringBuilder json = new StringBuilder();
+            while ((line = rd.readLine()) != null) {
+                json.append(line);
+            }
+
+            res = json.toString();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return res;
+    }
+
+    /**
+     *
+     * @param url
+     * @param params
+     * @param headerList
+     * @return
+     */
+    public String doPost(String url, List params, Map<String, String> headerList) {
+        String res = null;
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpPost post = new HttpPost(url);
+            if (headerList != null) {
+                headerList.entrySet().stream().forEach((t) -> {
+                    Header header = new BasicHeader(t.getKey(), t.getValue());
+                    post.setHeader(header);
+                });
+            }
+
+            if (params != null) {
+                //System.out.println((new UrlEncodedFormEntity(params)).toString());
+                post.setEntity(new UrlEncodedFormEntity(params));
+            }
+
+            HttpResponse response = client.execute(post);
+            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            String line = "";
+            StringBuilder json = new StringBuilder();
+            while ((line = rd.readLine()) != null) {
+                //System.out.println(line);
+                json.append(line);
+            }
+
+            res = json.toString();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return res;
+    }
+
+    /**
+     *
+     * @param url
+     * @param headerList
+     * @return
+     * @throws ParseException
+     */
+    public String doGet(String url, List params, Map<String, String> headerList) {
+        System.out.println("doGet");
+        String res = null;
+
+        if (params != null) {
+            StringBuilder pStr = new StringBuilder();
+            for (Object param : params) {
+                pStr.append(param.toString());
+                //System.out.println("param = " + URLEncoder.encode(param.toString()));
+            }
+            if (pStr.toString().length() > 0) {
+                url = url + "?" + pStr.toString();
+                //System.out.println("url = " + url);
+            }
+        }
+
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(url);
+        HttpResponse response;
+
+        if (headerList != null) {
+            headerList.entrySet().stream().forEach((t) -> {
+                Header header = new BasicHeader(t.getKey(), t.getValue());
+                request.setHeader(header);
+            });
+        }
+
+        try {
+            response = client.execute(request);
+            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+            String line = "";
+            StringBuilder json = new StringBuilder();
+            while ((line = rd.readLine()) != null) {
+                json.append(line);
+            }
+
+            //System.out.println("json = " + json.toString());
+            res = json.toString();
+            /*org.json.simple.parser.JSONParser parser = new JSONParser();
+            Object obj = parser.parse(json.toString());
+            try {
+                JSONObject jsonObj = (JSONObject) obj;
+                res = jsonObj;
+            } catch (Exception e) {
+                JSONArray jsonArr = (JSONArray) obj;
+                System.out.println("arr = " + jsonArr.toJSONString() );
+            }
+            System.out.println("res = " + res.toJSONString());*/
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return res;
+    }
+
+    /**
+     *
+     * @param url
+     * @param params
+     * @param headerList
+     * @return
+     */
+    public int doPut(/*String data,*/String url, Object params, Map<String, String> headerList) {
+        System.out.println("doPut => " + url + " param = " + params);
+        int responseCode = -1;
+        HttpClient httpClient = new DefaultHttpClient();
+        try {
+            HttpPut request = new HttpPut(url);
+
+            // Set PARAMS
+            if (params != null) {
+                Gson gson = new Gson();
+                StringEntity paramStr = new StringEntity(gson.toJson(params), "UTF-8");
+                request.setEntity(paramStr);
+            }
+
+            // Set HEADERS
+            if (headerList != null) {
+                headerList.entrySet().stream().forEach((t) -> {
+                    Header header = new BasicHeader(t.getKey(), t.getValue());
+                    request.setHeader(header);
+                });
+            }
+
+            HttpResponse response = httpClient.execute(request);
+            responseCode = response.getStatusLine().getStatusCode();
+            System.out.println("responseCode = " + responseCode);
+            if (response.getStatusLine().getStatusCode() == 200 || response.getStatusLine().getStatusCode() == 204) {
+                if (responseCode != 204) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent()), "UTF-8"));
+                    System.out.println("br = " + br);
+                    String output;
+                    // System.out.println("Output from Server ...." + response.getStatusLine().getStatusCode() + "\n");
+                    while ((output = br.readLine()) != null) {
+                        System.out.println(output);
+                    }
+                    System.out.println("output = " + output);
+                }
+            } else {
+                System.out.println(response.getStatusLine().getStatusCode());
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+            }
+
+        } catch (Exception ex) {
+            System.out.println("ex Code sendPut: " + ex);
+            System.out.println("url:" + url);
+            System.out.println("data:" + params);
+        } finally {
+            httpClient.getConnectionManager().shutdown();
+        }
+
+        return responseCode;
+
+    }
+}
