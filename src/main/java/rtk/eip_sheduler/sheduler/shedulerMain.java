bacef9ad3778200aaed8e028a4ac5dab29e3017d
vasil <andr_vasil@mail.ru>
2017-08-26 19:19:16 +0300

    Рабочая версия

--- src/main/java/rtk/eip_sheduler/sheduler/shedulerMain.java
+++ src/main/java/rtk/eip_sheduler/sheduler/shedulerMain.java
@@ -5,13 +5,19 @@
  */
 package rtk.eip_sheduler.sheduler;
 
+import java.io.StringReader;
 import java.net.URL;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 import rtk.eip_sheduler.DAO.TUsersDAO;
 import rtk.eip_sheduler.DAO.TUsersLogDAO;
+import static rtk.eip_sheduler.XMLUtil.utlXML.XMLtoString;
 import rtk.eip_sheduler.beans.TUsers;
 import rtk.eip_sheduler.beans.TUsersLog;
 import rtk.eip_sheduler.eipUtil.utlEip;
@@ -33,21 +39,30 @@ public class shedulerMain {
             log.info("Start");
             EntityManager em = Persistence.createEntityManagerFactory("EIP_shaduler_eip_sheduler_jar_1PU").createEntityManager();
             //em.setProperty(propertyName, log);
-            utlEip Eip = new utlEip(new URL("http://192.168.1.240"));
-            List<TUsersLog> logItems = (new TUsersLogDAO(em)).getList();            
+            utlEip Eip = new utlEip(new URL("http://192.168.1.150:8080/adminREST-1/webresources/userREST/getInfo"));
+            List<TUsersLog> logItems = (new TUsersLogDAO(em)).getList();
             for (TUsersLog item : logItems) {
                 log.info(item);
                 // отправка сообщения в ЕИП
                 // Получаем данные о пользователе
                 TUsers user = (new TUsersDAO(em)).getItem(item.getUserId());
                 log.debug(user);
-                Eip.addUser(user);
+                String res = Eip.addUser(user);
+
+                res = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                        + "<response resultCode =\"0\" resultComment=\"Логин создан автоматически\" user=\"petrov-elk-rtk\" dateTime=\"2013-05-08T12:39:00+06:00\"/>";
+
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(new InputSource(new StringReader(res)));
+                
+                log.debug(XMLtoString(doc));
                 // Если получен положительный ответ то ставим отметку об успешной отправке
                 item.setFlag(true);
                 (new TUsersLogDAO(em)).updateItem(item);
             }
 
-        } catch (Exception e) {            
+        } catch (Exception e) {
             log.error(e.getMessage());
         }
 
