7bd1da49646cb849fd975b9cafa63a4c281d3ac3
vasil <andr_vasil@mail.ru>
2017-09-07 19:49:15 +0300

    Рабочая версия

--- src/main/java/rtk/eip_sheduler/XMLUtil/utlXML.java
+++ src/main/java/rtk/eip_sheduler/XMLUtil/utlXML.java
@@ -17,10 +17,8 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 /**
  *
